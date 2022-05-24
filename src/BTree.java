89ab02a98adb189627d6235f6ada76b070699111
mariamtarek51 <mariamtarek51@gmail.com>
2022-05-24 03:12:56 +0200

    mar

--- src/BTree.java
+++ src/BTree.java
@@ -1,27 +1,29 @@
-public class BTree  <K extends Comparable<K>, V> implements IBTree {
+import javax.management.RuntimeErrorException;
+public class BTree  <K extends Comparable<K>, V> implements IBTree<K, V> {
+
+
     @Override
     public int getMinimumDegree() {
         return 0;
     }
 
     @Override
-    public IBTreeNode getRoot() {
+    public IBTreeNode<K, V> getRoot() {
         return null;
     }
 
     @Override
-    public void insert(Comparable key, Object value) {
+    public void insert(K key, V value) {
 
     }
 
     @Override
-    public Object search(Comparable key) {
-
+    public V search(K key) {
         return null;
     }
 
     @Override
-    public boolean delete(Comparable key) {
+    public boolean delete(K key) {
         return false;
     }
 }
